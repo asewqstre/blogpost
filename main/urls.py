5cc7491545f56fcbfd8a4abad61debc52f848dec
Aziz Mutallap <KZVAMUTA@na.intranet.msd>
2024-11-10 23:51:40 +0500

    Added delete post feature

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='Home'),
-    path('post-<int:pk>', views.PostsDetailed.as_view(), name="postsDetailed")
+    path('home/', views.home),
+    path('post-<int:pk>', views.PostsDetailed.as_view(), name="postsDetailed"),
+    path('post-<int:pk>/delete', views.DeletePost.as_view(), name="deletePost"),
 ]
--- main/views.py
+++ main/views.py
@@ -10,3 +10,9 @@ class PostsDetailed(generic.DetailView):
     model = models.Posts
     template_name = 'main/postsDetails.html'
     context_object_name = 'post'
+
+class DeletePost(generic.DeleteView):
+    model = models.Posts
+    template_name = 'main/deletePost.html'
+    context_object_name = 'post'
+    success_url = '/home/'
