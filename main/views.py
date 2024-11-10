1a7c4d49bace931857f95d047edec8998ec4b94c
Aziz Mutallap <KZVAMUTA@na.intranet.msd>
2024-11-10 20:46:58 +0500

    Created watching posts details feature and styled home a bit

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from . import models
+from django.views import generic
 
 def home(request):
     posts = models.Posts.objects.all()
     return render(request, 'main/home.html', {'posts':posts})
+
+class PostsDetailed(generic.DetailView):
+    model = models.Posts
+    template_name = 'main/postsDetails.html'
+    context_object_name = 'post'
